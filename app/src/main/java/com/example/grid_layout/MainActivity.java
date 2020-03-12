ea8c999c7023e3158b70f69cc8705a2ae9bb8c71
hassanatu <hassanatuahmed05@gmail.com>
2020-03-12 12:53:16 +0000

    first commit

---
+++ app/src/main/java/com/example/grid_layout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.grid_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void onImageClicked(View view) {
+        Toast.makeText(MainActivity.this, "item selected", Toast.LENGTH_LONG).show();
+//        Log.v(TAG, " click");
+    }
+
+    }
+
