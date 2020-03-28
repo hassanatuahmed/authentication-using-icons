c8fabe1f02e995e540b72967f77098d2e218c34e
hassanatu <hassanatuahmed05@gmail.com>
2020-03-28 10:02:39 +0000

    up

---
+++ app/src/main/java/com/example/grid_layout/LockScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.grid_layout;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.app_name), 0);
+        String list = sharedPref.getString(getString(R.string.preference_file_key),"");
+        if (list.isEmpty()){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
+
+        Gson gson = new Gson();
+        List<Model> modelListSave = gson.fromJson(list, new TypeToken<List<Model>>(){}.getType());
+        Log.d("qqq", modelListSave.toString());
+    }
+}
